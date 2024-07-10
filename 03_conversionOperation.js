e1b2121651e03b220db78c6d21fc8a77e03fc14a
amit-jha99 <amitjha121299@gmail.com>
2024-07-11 00:18:55 +0530

    Arithmetic Operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -23,5 +23,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber =24
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************ Operations *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 ="hello"
+let str2 ="  Amit Jha"
+let str3 =str1+str2 
+console.log(str3);
+
+console.log("1" +2);//12
+console.log(1 +"2");//12
+console.log("1" + 2 + 2);//122
+console.log(2+2+"1");//41
+
+// console.log(true)//true 
+// console.log(+true);//1
+// // console.log(true+) //Error expected
+// console.log(+"");//0
+
+let num1, num2, num3 
+num1=num2 =num3=2+2
+
+let gameCounter =100 
+console.log(++gameCounter)
+console.log(++gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
+// console.log(gameCounter);
