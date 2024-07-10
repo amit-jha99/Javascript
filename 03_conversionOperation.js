bb8b9ded5aa4dafa5a9a698596cc17de1956d078
amit-jha99 <amitjha121299@gmail.com>
2024-07-10 23:49:25 +0530

    conversion of data types

---
+++ 03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score ="33abc"
+// let score =null // will get 0 in valueInNumber .
+// let score =true // will get 1 in valueInNumber and 0 for false 
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)//NaN
+
+//"33" =>33
+// "33ab"=> NaN
+// true =>1; false =>0 
+
+let isLoggedIn =1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true; 0 0 => false
+// " "=> false
+// "Amit Jha"=>true
+
+let someNumber =24
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
