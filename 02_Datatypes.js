9e4396dd4c37eb56c6474be0d0e5a5aa95bafa64
amit-jha99 <amitjha121299@gmail.com>
2024-07-10 23:33:35 +0530

    Added Datatypes

---
+++ 02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not browser
+console.log(3
+    +
+    3) // code readability should be high
+console.log("AmitJha")
+
+
+
+let name ="Amit Jha"
+let age =24
+let isLoggedIn = true 
+
+//number => 2^53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => value not assigned yet
+//symbol => unique 
+
+//object
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
