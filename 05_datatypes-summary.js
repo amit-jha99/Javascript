0da8f5481c7eb5d22b50a8bb5a33dec91afcd93b
amit-jha99 <amitjha121299@gmail.com>
2024-07-16 19:28:41 +0530

    datatypes-summary

---
+++ 05_datatypes-summary.js
@@ -0,0 +1,40 @@
+// #Primitive(call by value)
+
+//7 types : String ,Number,Boolean ,null,undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue =100.3
+const isLoggedIn = false 
+const outsideTemp =null 
+let userEmail;
+
+const id=Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id);
+console.log(anotherId)
+console.log(id===anotherId)
+
+const bigNumber =346778861234567876545676n
+
+
+
+//Reference (Non primitve)(call by reference)
+//Array ,Objects,Functions
+
+const heros =["shaktiman","naagraj","doga"]
+let myObj={ 
+    name :"AmitJha",
+    age: 24,
+
+
+}
+
+const myFunction=function(){
+ console.log("Hello world");
+}
+
+console.log(typeof myObj);
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
